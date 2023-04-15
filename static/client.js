3985675a90f8905804a3fd671ed4fd182bb8dd22
Pratik Shinde <pratikshinde7262@gmail.com>
2023-04-15 22:19:53 +0530

    random

--- static/client.js
+++ static/client.js
@@ -1,5 +1,8 @@
 let btn=document.getElementById('btn');
+let inp=document.getElementById('inp')
 
-btn.addEventListener('click',()=>{
-    location.reload();
+btn.addEventListener('click',async ()=>{
+    let pas=await fetch('http://localhost:766/');
+    inp.value=pas;
 })
+
